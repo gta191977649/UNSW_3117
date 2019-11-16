7f6d190308e1df032732d21a0bc0cdb0f23ed231
Nurupo <Nurupo>
2019-11-16 17:19:40 +1100

    实现了前端数据可视化的对接

--- backend/webserv.py
+++ backend/webserv.py
@@ -7,7 +7,18 @@ import random
 app = Flask(__name__)
 CORS(app)
 
+time_slice_len = 60
+current_step = 0
+freq_data = {}
+freq_data["x"] = []
+freq_data["y"] = []
+
+shimmer_data = {}
+shimmer_data["x"] = []
+shimmer_data["y"] = []
+
 def generateRandom():
+    #Random generate states
     statechouse = random.randint(0,4)
     rand_state = None
     if(statechouse == 0):
@@ -20,8 +31,29 @@ def generateRandom():
         rand_state =  {'mid_strong': random.randint(0, 5)}
     elif(statechouse == 4):
         rand_state = {'strong': random.randint(0, 5)}
+    #Random generate charts data
+    #Generate Frequqnce Data in intervals
+    global current_step
+    current_step += 1
+    if(len(freq_data["x"]) > time_slice_len):
+        freq_data["x"].pop(0)
+        freq_data["y"].pop(0)
+
+    freq_data["x"].append(current_step)
+    freq_data["y"].append(random.randint(0, 5))
+
+    if(len(shimmer_data["x"]) > time_slice_len):
+        shimmer_data["x"].pop(0)
+        shimmer_data["y"].pop(0)
+
+    shimmer_data["x"].append(current_step)
+    shimmer_data["y"].append(random.randint(8, 10))
+
+
     data = {
         "state": rand_state,
+        "freq":freq_data,
+        "shimmer":shimmer_data,
     }
     return data
 @app.route("/")
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,99 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import ReactEcharts from 'echarts-for-react'
+import './App.css'
 const API = 'http://localhost:5000/'
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      weak:0,
-      mid_weak:0,
-      mid:0,
-      mid_strong:0,
-      strong:0,
-    
-      shimmer :{
-        x:[],
-        y:[],
+      weak: 0,
+      mid_weak: 0,
+      mid: 0,
+      mid_strong: 0,
+      strong: 0,
+      shimmer: {
+        x: [],
+        y: [],
       },
       freq: {
-        x:[],
-        y:[],
+        x: [],
+        y: [],
       },
-      error:false
+      error: false,
+      error_msg: null,
     }
     this.processdata = this.processdata.bind(this)
+  }
+  getFreqOptions(){
+    let freq_option = {
+      textStyle:{
+        color: 'rgba(255,107,107,1)',
+        textShadowColor: 'rgb(255,107,107)',
+        textShadowBlur: 2
+      },
+      xAxis: {
+        type: 'category',
+            data: this.state.freq.x
+      },
+      yAxis: {
+        type: 'value',
+        splitLine: {
+          lineStyle: {
+            color: 'rgba(255,107,107,0.3)',
+            shadowColor: 'rgb(255,107,107)',
+            shadowBlur: 5
+          }
+        }
+      },
+      series: [{
+        data: this.state.freq.y,
+        type: 'line',
+        smooth: true,
+        color: '#ff6b6b',
+        shadowColor: 'rgb(255,41,115)',
+        shadowBlur: 10,
+      }]
+    }
+    return freq_option
+  }
+  getShimmerOptions(){
+    let freq_option = {
+      textStyle:{
+        color: 'rgba(0,255,178,1)',
+        textShadowColor: 'rgba(0,255,178,1)',
+        textShadowBlur: 2
+      },
+      xAxis: {
+        type: 'category',
+        data: this.state.shimmer.x,
+
+      },
+      yAxis: {
+        type: 'value',
+        splitLine: {
+          lineStyle: {
+            color: 'rgba(0,255,178,0.3)',
+            shadowColor: 'rgba(0,255,178,1)',
+            shadowBlur: 5
+          }
+        }
+      },
+      series: [{
+        data: this.state.shimmer.y,
+        type: 'line',
+        color: 'rgba(0,255,178,1)',
+        shadowColor: 'rgb(0,255,178)',
+        shadowBlur: 10,
+        smooth: true,
+      }]
+    }
+    return freq_option
   }
   async processdata(data) {
     if(data) {
       this.setState({error:false})
-
       let sta = Object.keys(data["state"])[0]
-      console.log(data.state)
+
       let value = data["state"][sta]
       switch(sta) {
         case "weak": 
@@ -47,6 +112,19 @@ class App extends Component {
           this.setState({strong:this.state.strong+value})
           break
       }
+      //freq data
+      let freq = {
+        x: data["freq"]["x"],
+        y: data["freq"]["y"],
+      }
+      this.setState({freq:freq})
+      //shimmer data
+      let shimmer = {
+        x: data["shimmer"]["x"],
+        y: data["shimmer"]["y"],
+      }
+      this.setState({shimmer:shimmer})
+
     }
   }
   async componentDidMount() {
@@ -56,10 +134,12 @@ class App extends Component {
         fetch(API)
         .then(response => response.json())
         .then(data => this.processdata(data))
+            .catch(e => {
+              this.setState({error:true,error_msg:e.toString()})
+            })
       },1000) 
     } catch(e){
       console.log(e)
-      this.setState({error:true})
     }
   }
   render() {
@@ -71,10 +151,15 @@ class App extends Component {
       <h1>MID:{mid}</h1>
       <h1>MID_STRONG:{mid_strong}</h1>
       <h1>STRONG:{strong}</h1>
+
+      <ReactEcharts option={this.getFreqOptions()} />
+      <ReactEcharts option={this.getShimmerOptions()} />
+
     </React.Fragment>   
 
     const error = <React.Fragment>
       <h1>Net work error</h1>
+      <p>{this.state.error_msg}</p>
     </React.Fragment>
     return (
       <div className="App">
